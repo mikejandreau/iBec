5f094944a2ce66ab3d8a3ea227cffb6f5bb8b9a9
Mike Jandreau <mikejandreau@gmail.com>
2021-03-25 09:53:32 -0400

    Added condition for ARIA/toggle

--- js/script.js
+++ js/script.js
@@ -24,20 +24,23 @@ var contentToggler = document.querySelector('.open_card__content');
 var cardContent = document.querySelector('.card__content');
 
 // Set initial WAI-ARIA values for content section and toggle button
-contentToggler.setAttribute( 'aria-expanded', 'false' );
-cardContent.setAttribute( 'aria-expanded', 'false' );
-
-// Toggle content and set WAI-ARIA values when chevron is clicked
-contentToggler.onclick = function() {
-  if (hasClass(cardContent, 'card_open')) {
-    removeClass(contentToggler, 'card_open'); 
-    removeClass(cardContent, 'card_open');
+if (window.innerWidth < 1200) {
     contentToggler.setAttribute( 'aria-expanded', 'false' );
     cardContent.setAttribute( 'aria-expanded', 'false' );
-  } else {
-    addClass(contentToggler, 'card_open'); 
-    addClass(cardContent, 'card_open');
-    contentToggler.setAttribute( 'aria-expanded', 'true' );
-    cardContent.setAttribute( 'aria-expanded', 'true' );
-  }
-};
+
+    // Toggle content and set WAI-ARIA values when chevron is clicked
+    contentToggler.onclick = function() {
+      if (hasClass(cardContent, 'card_open')) {
+        removeClass(contentToggler, 'card_open'); 
+        removeClass(cardContent, 'card_open');
+        contentToggler.setAttribute( 'aria-expanded', 'false' );
+        cardContent.setAttribute( 'aria-expanded', 'false' );
+      } else {
+        addClass(contentToggler, 'card_open'); 
+        addClass(cardContent, 'card_open');
+        contentToggler.setAttribute( 'aria-expanded', 'true' );
+        cardContent.setAttribute( 'aria-expanded', 'true' );
+      }
+    };
+}
+
